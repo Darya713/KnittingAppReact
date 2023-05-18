919de189a179d526e44a7ee288a599e09e5a68cd
Darya Krauchuk <Darya_Krauchuk@epam.com>
2023-05-18 23:26:40 +0200

    replaced strictmode to browserrouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import Main from './features/main';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <Main />
-    </React.StrictMode>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
