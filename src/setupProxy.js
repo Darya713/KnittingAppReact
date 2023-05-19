fbfca315aa20ce5360a1f9740acdca1943769505
Darya Krauchuk <Darya_Krauchuk@epam.com>
2023-05-19 11:58:36 +0200

    Update setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const context = ['/knittingtypes'];
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
         target: 'https://knittingapp-backend.azurewebsites.net/',
-        secure: false,
+        changeOrigin: true,
     });
 
     app.use(appProxy);
