7fbca3645aa0576802416e29356778951c45161f
Darya Krauchuk <Darya_Krauchuk@epam.com>
2023-05-22 14:48:17 +0200

    remove app using

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import './assets/styles/index.css';
 import Main from './features/main';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +8,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App />
         <Main />
     </BrowserRouter>
 );
